2dacd10acf477708ab60d32ad4864edd5b43be44
sergiogalici <103759691+sergiogalici@users.noreply.github.com>
2023-01-25 06:37:40 +0100

    1 file modified

--- index.js
+++ index.js
@@ -22,11 +22,17 @@ const createTicket = (logo, flight) => {
   const ticketWrapper = document.createElement('div');
   const firstRow = document.createElement('div');
   const secondRow = document.createElement('div');
-  const logoImg = document.createElement("div")
+  const logoDiv = document.createElement('div');
 
   ticketWrapper.classList.add('tickets-container__ticket');
   firstRow.classList.add('tickets-container__ticket--first-row');
   secondRow.classList.add('tickets-container__ticket--second-row');
+  logoDiv.innerHTML += "<img src="https://loghi-famosi.com/wp-content/uploads/2021/11/Logo-della-ITA-Airways.png" alt="Girl in a jacket">"
+  ticketWrapper.appendChild(firstRow)
+  ticketWrapper.appendChild(secondRow)
+  ticketWrapper.appendChild(logoDiv)
+  appDiv.appendChild(ticketWrapper)
 };
 
 createWrapper();
+createTicket()
